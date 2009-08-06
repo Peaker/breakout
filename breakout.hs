297e0268f75b24f52c97ef5caa07671d4fe8d17f
Eyal Lotem <eyal.lotem@gmail.com>
2009-08-06 10:50:27 +0300

    Add brick collisions

--- breakout.hs
+++ breakout.hs
@@ -2,197 +2,211 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Main where
-    import qualified Graphics.UI.HaskGame as HaskGame
-    import qualified Graphics.UI.HaskGame.Vector2 as Vector2
-    import Graphics.UI.HaskGame.Vector2(Vector2(..))
-    import Graphics.UI.HaskGame.Color(Color(..))
-    import qualified Graphics.UI.SDL as SDL
-    import Graphics.UI.SDL(Rect(..))
-    import qualified IO
-    import qualified Control.Exception as Exception
-    import Control.Exception(Exception)
-    import Data.Typeable(Typeable)
-    import Control.Monad(forM_, forever, when)
-    import Control.Monad.State.Strict(evalStateT, modify, get)
-    import Control.Monad.Trans(liftIO)
-    import Control.Applicative((<$>))
 
-    data QuitException = QuitException
-      deriving (Show, Typeable)
-    instance Exception QuitException where
-      -- Nothing
+import qualified Graphics.UI.HaskGame as HaskGame
+import qualified Graphics.UI.HaskGame.Vector2 as Vector2
+import Graphics.UI.HaskGame.Vector2(Vector2(..))
+import Graphics.UI.HaskGame.Color(Color(..))
+import qualified Graphics.UI.SDL as SDL
+import Graphics.UI.SDL(Rect(..))
+import qualified IO
+import qualified Control.Exception as Exception
+import Control.Exception(Exception)
+import Data.Typeable(Typeable)
+import Data.Maybe(listToMaybe, isNothing, mapMaybe)
+import Control.Monad(forM_, forever, when)
+import Control.Monad.State.Strict(evalStateT, modify, get)
+import Control.Monad.Trans(liftIO)
+import Control.Applicative((<$>))
 
-    capRange :: Ord a => a -> a -> a -> a
-    capRange bottom top x = (x `max` bottom) `min` top
+data Direction = X | Y
+  deriving (Eq, Ord, Show, Read)
 
-    brickColor :: Color
-    brickColor = Color 255 0 0
-    brickWidth :: Int
-    brickWidth = 72
-    brickHeight :: Int
-    brickHeight = 30
-    spaceWidth :: Int
-    spaceWidth = 8
-    spaceHeight :: Int
-    spaceHeight = 5
-    ballRadius :: Int
-    ballRadius = 8
+data QuitException = QuitException
+  deriving (Show, Typeable)
+instance Exception QuitException where
+  -- Nothing
 
-    playerColor :: Color
-    playerColor = Color 0 0 255
+displayWidth :: Int
+displayHeight :: Int
+displaySize :: Vector2 Int
+displaySize@(Vector2 displayWidth displayHeight) = Vector2 640 480
+colordepth :: Int
+colordepth = 32
 
-    playerHeight :: Int
-    playerHeight = 15
-    initialPlayerWidth :: Int
-    initialPlayerWidth = 80
-    initialBallSpeed :: Vector2 Double
-    initialBallSpeed = Vector2 1 (-2)
+capRange :: Ord a => a -> a -> a -> a
+capRange bottom top x = (x `max` bottom) `min` top
 
-    playerRectAt :: Int -> Int -> Int -> Rect
-    playerRectAt height x width =
-        Rect (x - hwidth) (height - playerHeight)
-             width height
-        where
-          hwidth = width `div` 2
+bgColor :: Color
+bgColor = Color 0 0 0
 
-    capPlayerRange :: Int -> Int -> Int -> Int
-    capPlayerRange displayWidth curPlayerWidth = capRange halfWidth (displayWidth - halfWidth)
-        where halfWidth = curPlayerWidth `div` 2
+brickColor :: Color
+brickColor = Color 255 0 0
+brickWidth :: Int
+brickWidth = 72
+brickHeight :: Int
+brickHeight = 30
+spaceWidth :: Int
+spaceWidth = 8
+spaceHeight :: Int
+spaceHeight = 5
+ballRadius :: Int
+ballRadius = 8
 
-    data GameState = GameState {
-              gsPlayerPos :: Int
-            , gsPlayerWidth :: Int
-            , gsBallPosSpeed :: Maybe (Vector2 Int, Vector2 Double)
-            , gsBrickPositions :: [Vector2 Int]
-            }
+playerColor :: Color
+playerColor = Color 0 0 255
 
-    type Endo a = (a -> a)
-    type Inside whole part = Endo part -> Endo whole
-    atGsPlayerPos :: Inside GameState Int
-    atGsPlayerPos f gs = gs{gsPlayerPos = f (gsPlayerPos gs)}
-    atGsPlayerWidth :: Inside GameState Int
-    atGsPlayerWidth f gs = gs{gsPlayerWidth = f (gsPlayerWidth gs)}
-    atGsBallPosSpeed :: Inside GameState (Maybe (Vector2 Int, Vector2 Double))
-    atGsBallPosSpeed f gs = gs{gsBallPosSpeed = f (gsBallPosSpeed gs)}
-    atGsBrickPositions :: Inside GameState [Vector2 Int]
-    atGsBrickPositions f gs = gs{gsBrickPositions = f (gsBrickPositions gs)}
+playerHeight :: Int
+playerHeight = 15
+initialPlayerWidth :: Int
+initialPlayerWidth = 80
+initialBallSpeed :: Vector2 Double
+initialBallSpeed = Vector2 1 (-2)
 
-    -- Pure part
-    brickPositionsIn :: Vector2 Int -> [Vector2 Int]
-    brickPositionsIn (Vector2 width height) =
-        [Vector2 x y
-         | x <- [0, brickWidth+spaceWidth..width-brickWidth]
-        , y <- [0, brickHeight+spaceHeight..height-brickHeight]]
+playerRect :: GameState -> Rect
+playerRect GameState{gsPlayerPos=playerPos
+                    ,gsPlayerWidth=playerWidth} =
+    Rect (playerPos - halfWidth) (displayHeight - playerHeight)
+         playerWidth displayHeight
+    where
+      halfWidth = playerWidth `div` 2
 
+capPlayerRange :: Int -> Int -> Int
+capPlayerRange curPlayerWidth = capRange halfWidth (displayWidth - halfWidth)
+    where halfWidth = curPlayerWidth `div` 2
 
-    brickArea :: Vector2 Int -> Vector2 Int
-    brickArea (Vector2 width height) = Vector2 width (height * 2 `div` 5)
+data GameState = GameState {
+          gsPlayerPos :: Int
+        , gsPlayerWidth :: Int
+        , gsBallPosSpeed :: Maybe (Vector2 Double, Vector2 Double)
+        , gsBrickPositions :: [Vector2 Int]
+        }
 
-    initBrickPositions :: Vector2 Int -> [Vector2 Int]
-    initBrickPositions = brickPositionsIn . brickArea
+type Endo a = (a -> a)
+type Inside whole part = Endo part -> Endo whole
+atGsPlayerPos :: Inside GameState Int
+atGsPlayerPos f gs = gs{gsPlayerPos = f (gsPlayerPos gs)}
+atGsPlayerWidth :: Inside GameState Int
+atGsPlayerWidth f gs = gs{gsPlayerWidth = f (gsPlayerWidth gs)}
+atGsBallPosSpeed :: Inside GameState (Maybe (Vector2 Double, Vector2 Double))
+atGsBallPosSpeed f gs = gs{gsBallPosSpeed = f (gsBallPosSpeed gs)}
+atGsBrickPositions :: Inside GameState [Vector2 Int]
+atGsBrickPositions f gs = gs{gsBrickPositions = f (gsBrickPositions gs)}
 
-    brickRect :: Vector2 Int -> Rect
-    brickRect (Vector2 x y) = Rect x y brickWidth brickHeight
+initBrickPositions :: [Vector2 Int]
+initBrickPositions =
+    let height = displayHeight * 2 `div` 5
+    in [Vector2 x y
+       | x <- [0, brickWidth+spaceWidth..displayWidth-brickWidth]
+       , y <- [0, brickHeight+spaceHeight..height-brickHeight]]
 
-    -- Monadic part
-    handleEvents :: [SDL.Event] -> IO ()
-    handleEvents events = do
-      forM_ events $ \event -> case event of 
-                                       SDL.Quit -> Exception.throwIO QuitException
-                                       _ -> return ()
+brickRect :: Vector2 Int -> Rect
+brickRect (Vector2 x y) = Rect x y brickWidth brickHeight
 
-    draw :: SDL.Surface -> Vector2 Int -> Rect -> [Vector2 Int] -> IO ()
-    draw display (Vector2 ballx bally) playerRect brickPositions = do
-        forM_ brickPositions $ \pos ->
-            HaskGame.fillRect display (brickRect pos) brickColor
-        HaskGame.fillRect display playerRect playerColor
-        let ballRect = Rect (ballx - ballRadius) (bally - ballRadius) (ballRadius*2) (ballRadius*2)
-        HaskGame.fillRect display ballRect playerColor
+handleEvents :: [SDL.Event] -> IO ()
+handleEvents events = do
+  forM_ events $ \event -> case event of 
+                                   SDL.Quit -> Exception.throwIO QuitException
+                                   _ -> return ()
 
-    inRange :: Ord a => a -> a -> a -> Bool
-    inRange bottom top x = x >= bottom && x < top
+draw :: SDL.Surface -> GameState -> IO ()
+draw display gs = do
+    forM_ (gsBrickPositions gs) $ \pos ->
+        HaskGame.fillRect display (brickRect pos) brickColor
+    let Vector2 ballx bally = ballPosition gs
+    HaskGame.fillRect display (playerRect gs) playerColor
+    let ballRect = Rect (truncate ballx - ballRadius) (truncate bally - ballRadius) (ballRadius*2) (ballRadius*2)
+    HaskGame.fillRect display ballRect playerColor
 
-    nextGameStateIn :: Vector2 Int -> GameState -> GameState
-    nextGameStateIn (Vector2 width height) gs = maybe gs newState $ gsBallPosSpeed gs
+inRange :: Ord a => a -> a -> a -> Bool
+inRange bottom top x = x >= bottom && x < top
+
+nextGameState :: GameState -> GameState
+nextGameState gs =
+    maybe gs newState $ gsBallPosSpeed gs
+  where
+    Rect px _ pw _ = playerRect gs
+    brickPositions = gsBrickPositions gs
+    newState (ballPos, ballSpeed@(Vector2 speedX speedY)) =
+        gs{gsBallPosSpeed=if ballY >= fi displayHeight
+                          then Nothing
+                          else Just (ballPos', ballSpeed')
+          ,gsBrickPositions=filter (isNothing . collision) $ brickPositions }
       where
-        brickPoss = gsBrickPositions gs
-        playerWidth = gsPlayerWidth gs
-        Rect px _ pw _ = playerRectAt height (gsPlayerPos gs) playerWidth
-        newState (ballPos, ballSpeed@(Vector2 sx sy)) =
-            gs{gsBallPosSpeed=if offScreen
-                              then Nothing
-                              else Just (ballPos', ballSpeed')
-              ,gsBrickPositions=filter noCollision brickPoss }
-          where
-            fi = fromIntegral
-            noCollision = not . collide ballPos' . brickRect
-            ballPos'@(Vector2 bpx bpy) = ballPos + (truncate <$> ballSpeed)
-            hitBottom = bpy >= height
-            offScreen = hitBottom && not (bpx >= px && bpx <= px+pw)
-            factor = if hitBottom
-                     then (+) $ (8::Double) * (fi bpx - fi px - fi pw/2) / fi pw
-                     else id
+        ballPos'@(Vector2 ballX ballY) = ballPos + ballSpeed
+        collision = collideBall (truncate <$> ballPos') . brickRect
+        firstCollision = listToMaybe $ mapMaybe collision brickPositions
 
-            ballSpeed' =
-                Vector2
-                (factor $ if inRange 0 width bpx
-                          then sx
-                          else -sx)
-                (if inRange 0 height bpy
-                 then sy
-                 else -sy)
+        fi = fromIntegral
+        hitPlayer = ballY >= fi (displayHeight - playerHeight) &&
+                    ballX >= fi px && ballX <= fi (px+pw)
+        factor = if hitPlayer
+                 then (+) $ 8 * (ballX - fi px - fi pw/2) / fi pw
+                 else id
+        collideX = maybe False (==X) firstCollision
+        collideY = maybe False (==Y) firstCollision
 
-    collide :: Vector2 Int -> Rect -> Bool
-    collide (Vector2 px py) (Rect x y w h) = not $
-        (px + ballRadius) < x ||   -- ball is entirely left of rect
-        (px - ballRadius) > x+w || -- ball is entirely right of rect
-        (py + ballRadius) < y ||   -- ball is entirely up of rect
-        (py - ballRadius) > y+h    -- ball is entirely below rect
+        ballSpeed' =
+            Vector2
+            (factor $
+             if inRange 0 (fi displayWidth) ballX && not collideX
+             then speedX
+             else -speedX)
+            (if ballY >= 0 && not hitPlayer && not collideY
+             then speedY
+             else -speedY)
 
-    mainLoop :: SDL.Surface -> IO ()
-    mainLoop display = do
-      black <- SDL.mapRGB (SDL.surfaceGetPixelFormat display) 0 0 0
+collideBall :: Vector2 Int -> Rect -> Maybe Direction
+collideBall (Vector2 ballx bally) (Rect x y w h) =
+    let left  = ballx + ballRadius < x
+        right = ballx - ballRadius > x+w
+        up    = bally + ballRadius < y
+        down  = bally - ballRadius > y+h
+    in if left || right || up || down
+    then Nothing
+    else Just $
+         if ballx < x || ballx > x+w
+         then X
+         else Y
 
-      let displaySize@(Vector2 displayWidth displayHeight) = HaskGame.surfaceSize display
-          capPlayerRange' = capPlayerRange displayWidth
-          playerRectAt' = playerRectAt displayHeight
-          nextGameState = nextGameStateIn displaySize
+ballPosition :: GameState -> Vector2 Double
+ballPosition GameState{gsBallPosSpeed=ballPosSpeed
+                      ,gsPlayerPos=playerPos} =
+    maybe (fromIntegral <$> Vector2 playerPos (displayHeight - playerHeight - ballRadius))
+          fst $
+    ballPosSpeed
 
-      (`evalStateT` (GameState 0 initialPlayerWidth Nothing (initBrickPositions displaySize))) . forever $ do
-        liftIO $ SDL.fillRect display Nothing black
+sdlIteration :: SDL.Surface -> GameState -> IO ()
+sdlIteration display gs =
+  do
+    HaskGame.fillSurface display bgColor
+    draw display gs
+    SDL.flip display
+    HaskGame.getEvents >>= handleEvents
+    ticks <- SDL.getTicks
+    SDL.delay (10 - (ticks `mod` 10))
 
-        (mouseX, _, buttons) <- liftIO $ SDL.getMouseState
-        let leftPressed = SDL.ButtonLeft `elem` buttons
+mainLoop :: SDL.Surface -> IO ()
+mainLoop display = do
+  (`evalStateT` (GameState 0 initialPlayerWidth Nothing initBrickPositions)) . forever $ do
+    (mouseX, _, buttons) <- liftIO $ SDL.getMouseState
+    let leftPressed = SDL.ButtonLeft `elem` buttons
 
-        curPlayerWidth <- gsPlayerWidth <$> get
-        modify . atGsPlayerPos . const $ capPlayerRange' curPlayerWidth mouseX
-        curPlayerPos <- gsPlayerPos <$> get
+    gameState <- get
+    liftIO $ sdlIteration display gameState
+    let curPlayerWidth = gsPlayerWidth gameState
+        curPlayerPos = capPlayerRange curPlayerWidth mouseX
+    modify . atGsPlayerPos . const $ curPlayerPos
+    ballPos <- ballPosition `fmap` get
+    when leftPressed $
+         modify . atGsBallPosSpeed . const $ Just (ballPos, initialBallSpeed)
 
-        ballPos <- (maybe (Vector2 mouseX (displayHeight - playerHeight - ballRadius)) fst . gsBallPosSpeed)
-                   `fmap` get
-        modify nextGameState
-        when leftPressed $
-             modify . atGsBallPosSpeed $ const (Just (ballPos, initialBallSpeed))
-        brickPositions <- gsBrickPositions `fmap` get
-        liftIO $ draw display ballPos (playerRectAt' curPlayerPos curPlayerWidth) brickPositions
+    modify nextGameState
 
-        liftIO $ SDL.flip display
-        events <- liftIO $ HaskGame.getEvents
-        liftIO $ handleEvents events
-        ticks <- liftIO $ SDL.getTicks
-        liftIO $ SDL.delay (10 - (ticks `mod` 10))
-
-    xres :: Int
-    xres = 640
-    yres :: Int
-    yres = 480
-    colordepth :: Int
-    colordepth = 32
-
-    main :: IO ()
-    main = do
-      HaskGame.withInit $ do
-        display <- HaskGame.setVideoMode xres yres colordepth
-        mainLoop display
-          `Exception.catch`
-          \QuitException -> return ()
+main :: IO ()
+main = do
+  HaskGame.withInit $ do
+    display <- HaskGame.setVideoMode displaySize colordepth
+    mainLoop display
+      `Exception.catch`
+      \QuitException -> return ()
